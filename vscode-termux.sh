a069015ca858550d09d80f56936b7f93a7041dec
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2023-11-13 20:49:54 +0700

    Update vscode-termux.sh

--- vscode-termux.sh
+++ vscode-termux.sh
@@ -4,7 +4,4 @@ apt upgrade -y
 apt install tur-repo -y
 apt install code-server -y
 mkdir ~/.config/code-server
-echo bind-addr: 127.0.0.1:8900  > ~/.config/code-server/config.yaml
-echo auth: none  > ~/.config/code-server/config.yaml
-echo password: 123  > ~/.config/code-server/config.yaml
-echo cert: false > ~/.config/code-server/config.yaml
+echo bind-addr: 127.0.0.1:8900\nauth: none\npassword: 123\ncert: false > ~/.config/code-server/config.yaml
