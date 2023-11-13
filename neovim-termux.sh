f6ba9a5b4bc9b1d8e612f06bd9a108d446dda2be
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2023-11-13 20:11:53 +0700

    Create neovim-termux.sh

---
+++ neovim-termux.sh
@@ -0,0 +1,9 @@
+echo "Setup storage"
+termux-setup-storage
+
+echo "Update/upgrade package and installing depedencies that needed"
+apt update -y && apt upgrade -y && pkg install git neovim ripgrep nodejs-lts clang lua-language-server -y
+echo "Finished installation!"
+
+echo "Now clone the repo and open neovim"
+git clone https:/github.com/DinkyTrady/nvim --depth=1 ~/.config/nvim && nvim
