4d0bb14943cb82fbb6275bf7fae333bab9ca5abd
DinkyTrady <111265264+DinkyTrady@users.noreply.github.com>
2023-11-20 14:18:10 +0700

    Update neovim-termux.sh

--- neovim-termux.sh
+++ neovim-termux.sh
@@ -2,7 +2,7 @@ echo "Setup storage"
 termux-setup-storage
 
 echo "Update/upgrade package and installing depedencies that needed"
-apt update -y && apt upgrade -y && pkg install git neovim ripgrep nodejs-lts clang lua-language-server -y
+apt update -y && apt upgrade -y && apt install git neovim ripgrep nodejs-lts clang lua-language-server -y
 echo "Finished installation!"
 
 echo "Now clone the repo and open neovim"
